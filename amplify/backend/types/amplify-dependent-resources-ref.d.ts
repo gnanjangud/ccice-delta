98beb018ee7aac11f42a35c276f608a41f20d083
Nanjangud Srinivasarao, Girish <girish.nanjangud@delta.com>
2023-07-26 17:36:21 +0530

    React

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ccicedeltaapp2f27d02c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
